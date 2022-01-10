c421a2be0ce29f01002f2e9775a5a417b2eac349
Joan Rasmussen <raz.joan@gmail.com>
2022-01-10 09:49:55 -0700

    Move fetch call for specific drink

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,6 +3,11 @@ const apiCalls = {
     getGameData: () => {
         return fetch('https://api.boardgameatlas.com/api/search?name=Catan&pretty=true&client_id=JLBr5npPhV')
             .then(res => res.json())
+    },
+
+    getSpecificDrink: (drinkId) => {
+        return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
+            .then(res => res.json())
     }
 }
 
