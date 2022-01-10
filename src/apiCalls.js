2a1bcfd70dad588532125a9c2bf0d627a10b75ad
Joan Rasmussen <raz.joan@gmail.com>
2022-01-10 16:52:40 -0700

    Update apiCalls with throw Error and catch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,17 +2,38 @@ const apiCalls = {
 
     getGameData: () => {
         return fetch('https://api.boardgameatlas.com/api/search?limit=100&pretty=true&client_id=YWBLPm0lyY')
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error('Server is down, try again later!')
+                }
+            })
+            .catch(err => err)
     },
 
     getDrinkByType: (type) => {
         return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${type}`)
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error('Server is down, try again later!')
+                }
+            })
+            .catch(err => err)
     },
 
     getSpecificDrink: (drinkId) => {
         return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
-            .then(res => res.json())
+            .then(res => {
+                if (res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error('Server is down, try again later!')
+                }
+            })
+            .catch(err => err)
     }
 }
 
