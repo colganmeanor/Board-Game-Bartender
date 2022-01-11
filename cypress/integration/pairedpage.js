87c4dd894a90daad530dd68a8e35b35e204b50bf
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-10 21:05:50 -0600

    Add fixture file for sample cocktail in cypress tests

--- cypress/integration/pairedpage.js
+++ cypress/integration/pairedpage.js
@@ -2,16 +2,30 @@ describe('Board Game Bartender Paired Page DOM', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
         
-        cy.get('label').eq(0)
-            .should('contain', 'Choose Your Game!')
+        cy.get('input')
+            .should('have.length', 2)
 
-        cy.get('label').eq(1)
-            .should('contain', 'Whatchya drinking?')
+        cy.get('input').eq(0)
+            .should('have.attr', 'placeholder', 'Choose Your Game!')
+
+        cy.get('input').eq(1)
+            .should('have.attr', 'placeholder', 'Whatchya drinking?')
     })
 
-    it.only('should show the drink component', () => {
-        cy.get('[data-cy=header')
-            .should('exist')
+    it('should allow the user to access the paired page after clicking the Pair Button', () => {
+        cy.get('input').eq(0)
+          .type('Catan')
+
+          .get('input').eq(1)
+          .type('Tequila')
+
+          .get('[data-cy=pairing-form] [data-cy=pair-button]')
+          .click()
+
+          cy.intercept('GET', 'api/json/v1/1/lookup.php?i=178347', {
+            fixture: 'sampleDrink.json'
+          })
+          .url('http://localhost:3000/OIXt3DmJU0/178347')
     })
 
     
