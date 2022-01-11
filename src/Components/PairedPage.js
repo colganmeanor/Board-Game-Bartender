6893e30dda148a6e5e172a767e2ae4aa734fad44
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-11 17:59:16 -0600

    Add aria label to div in PairedPage

--- src/Components/PairedPage.js
+++ src/Components/PairedPage.js
@@ -43,7 +43,7 @@ const PairedPage = () => {
             { error ? <p className='error-message-pair-page'>Sorry, there's been an error: '{error}'</p> :
                 <main className="paired-page">
                     <h2 className='perfect-pairing-title'>Your Perfect Pairing!</h2>
-                    <div className='paired-components'>
+                    <div aria-label="Paired Drink and Game Results" className='paired-components'>
                         {game && <Game />}
                         <p className='plus-sign'>+</p>
                         {currentDrink && <Drink />}
